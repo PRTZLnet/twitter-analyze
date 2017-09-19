8fa5bb1b4199b8f343dcb5e19607388b67c4cb45
Quinn Brown <PRTZLnet@users.noreply.github.com>
2017-09-18 23:24:21 -0400

    Update cfg.php

--- cfg.php
+++ cfg.php
@@ -1,7 +1,7 @@
 <?php
  
 return [
-    //
+    // these api keys don't work, so go ahead and try lol
     'consumer_key'      => 'ZptcrxHFhDZsphYeGezhD4kJh',
     'consumer_secret'   => 'SUtmtVldq4FsgVOztz3imMnM8r59Oe29LceGi38o3xXEmTy0CN',
  
