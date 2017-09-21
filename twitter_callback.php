85f2c364f037df58c7e13475c3053ecf4c95c0fc
Quinn Brown <PRTZLnet@users.noreply.github.com>
2017-09-20 23:29:17 -0400

    Update twitter_callback.php

--- twitter_callback.php
+++ twitter_callback.php
@@ -1,12 +1,9 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
-
 use Abraham\TwitterOAuth\TwitterOAuth;
 use DarrynTen\GoogleNaturalLanguagePhp\GoogleNaturalLanguage;
-
 session_start();
 $config = require_once 'cfg.php';
-
 $oauth_verifier = filter_input(INPUT_GET, 'oauth_verifier');
 if (empty($oauth_verifier) ||
     empty($_SESSION['oauth_token']) ||
@@ -38,87 +35,53 @@ if(isset($user->error)) {
 // fuck pdo tbh
 $test = $d->get('account/settings');
 $usr = $test->screen_name;
-
 $config = [
   'projectId' => 'personal-cloud-155618'  // At the very least
 ];
-
-
 $language = new GoogleNaturalLanguage($config);
-
 $url = 'https://api.twitter.com/1.1/statuses/user_timeline.json';
-$getfield = '?screen_name=' . $usr . '&count=7';
+$getfield = '?screen_name=' . $usr . '&include_rts=false';
 $requestMethod = 'GET';
-
 
 $settings = array(
     'oauth_access_token' => $token['oauth_token'],
     'oauth_access_token_secret' => $token['oauth_token_secret'],
-    'consumer_key' =>  'ZptcrxHFhDZsphYeGezhD4kJh',
-    'consumer_secret' => 'SUtmtVldq4FsgVOztz3imMnM8r59Oe29LceGi38o3xXEmTy0CN',
+    'consumer_key' =>  'x',
+    'consumer_secret' => 'x',
 );
 
 $twitter = new TwitterAPIExchange($settings);
 $varto = $twitter->setGetfield($getfield)
     ->buildOauth($url, $requestMethod)
     ->performRequest();
-
-function indent($varto) {
-
-    $result      = '';
-    $pos         = 0;
-    $strLen      = strlen($varto);
-    $indentStr   = '  ';
-    $newLine     = "\n";
-    $prevChar    = '';
-    $outOfQuotes = true;
-
-    for ($i=0; $i<=$strLen; $i++) {
-        $char = substr($varto, $i, 1);
-        if ($char == '"' && $prevChar != '\\') {
-            $outOfQuotes = !$outOfQuotes;
-        }
-				else if(($char == '}' || $char == ']') && $outOfQuotes) {
-            $result .= $newLine;
-            $pos --;
-            for ($j=0; $j<$pos; $j++) {
-                $result .= $indentStr;
-            }
-        }
-        $result .= $char;
-        if (($char == ',' || $char == '{' || $char == '[') && $outOfQuotes) {
-            $result .= $newLine;
-            if ($char == '{' || $char == '[') {
-                $pos ++;
-            }
-
-            for ($j = 0; $j < $pos; $j++) {
-                $result .= $indentStr;
-            }
-        }
-
-        $prevChar = $char;
-    }
-
-    return $result;
-}
-
-file_put_contents('cache.json', indent($varto));
-?>
-
-<?php
 $r = 0;
 $g = 0;
-$json = file_get_contents('cache.json');
-$json_output = json_decode($json);
-//>implying I know what im doing 
-foreach ($json_output as $result) {
-    
-    $text = $result->text;
+$c = 0;
+$json_output = json_decode($varto);
+
+//because I'm a cheap shitbag Google's API limits me to ~1000 characters per request before charging me money. 7*140 = 980 
+
+$array = array(
+                  array( 'tweet' => 'null','s' => null ),
+                  array( 'tweet' => 'null','s' => null ),
+                  array( 'tweet' => 'null','s' => null ),
+                  array( 'tweet' => 'null','s' => null ),
+                  array( 'tweet' => 'null','s' => null ),
+                  array( 'tweet' => 'null','s' => null ),
+                  array( 'tweet' => 'null','s' => null ),
+
+              );
+              
+foreach($json_output as $result) {
+$text = $result->text;
+//this^^^ is required. It would be kinda nice if PHP let you do $json_output->text as $result. Maybe it can and im bad at formatting.
 $language->setText($text);
 $annotation = $language->getSentiment();
 $sentiment = $annotation->sentiment();
 if($sentiment['score'] != null) {
+$tempvar1 = $sentiment['score'];
+$array[$g]['tweet'] = $text;
+$array[$g]['s'] = $tempvar1;
     $r+=$sentiment['score'];
     echo $r . " ";
     $g++;
@@ -127,64 +90,4 @@ if($sentiment['score'] != null) {
 }
 $r = ($r / $g);
 echo 'final =' . $r;
-if ($r==1){
-    $status = $d->post(
-    "statuses/update", [
-        "status" => "according to http://twitter.prtzl.net my twitter is only positive!(".($r*100)."%)"
-    ]
-);
-}
-else if ($r > .6){
-        $status = $d->post(
-    "statuses/update", [
-        "status" => "according to http://twitter.prtzl.net my twitter is very positive!(".($r*100)."%)"
-    ]
-);
-}
-else if ($r > .3){
-    $status = $d->post(
-    "statuses/update", [
-        "status" => "according to http://twitter.prtzl.net my twitter is mostly positive!(".($r*100)."%)"
-    ]
-);}
-else if ($r > 0){
-    $status = $d->post(
-    "statuses/update", [
-        "status" => "according to http://twitter.prtzl.net my twitter is sorta positive!(".($r*100)."%)"
-    ]
-);}
-else if ($r == 0){
-    $status = $d->post(
-    "statuses/update", [
-        "status" => "according to http://twitter.prtzl.net my twitter is totally neutral!(".($r*100)."%)"
-    ]
-);}
-else if ($r== -1){
-    $status = $d->post(
-    "statuses/update", [
-        "status" => "according to http://twitter.prtzl.net my twitter is only negative!(".($r*100)."%)"
-    ]
-);}
-else if ($r < -.6){
-    $status = $d->post(
-    "statuses/update", [
-        "status" => "according to http://twitter.prtzl.net my twitter is very negative!(".($r*100)."%)"
-    ]
-);}
-else if ($r < -.3){
-    $status = $d->post(
-    "statuses/update", [
-        "status" => "according to http://twitter.prtzl.net my twitter is mostly negative!(".($r*100)."%)"
-    ]
-);}
-else if ($r > -.3){
-    $status = $d->post(
-    "statuses/update", [
-        "status" => "according to http://twitter.prtzl.net my twitter is sorta negative!(".($r*100)."%)"
-    ]
-);}
-
-$url = "https://twitter.com";
-header('Location: '. $url);
- 
-?>
+print_r($array);
